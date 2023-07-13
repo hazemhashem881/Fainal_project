2179c4e65d8833ed5c231ddb9d332687b213bfec
Hazem Hashem <hazemhashem100@gmail.com>
2023-07-13 16:11:03 +0300

    update k8s

--- k8s/apply.sh
+++ k8s/apply.sh
@@ -4,5 +4,5 @@ kubectl apply -f jenkinspvc.yml
 kubectl apply -f jenkins-service.yml
 kubectl apply -f jenkinsaccount.yml
 kubectl apply -f jenkinsdeployment.yml
-kubectl apply -f jenkins-slave.yml
 kubectl apply -f jenkins-slave-svc.yaml
+kubectl apply -f jenkins-slave-deploy.yml
